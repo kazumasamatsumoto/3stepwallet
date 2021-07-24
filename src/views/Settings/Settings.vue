4edad67ca821240674f83c543b5286348f77654a
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-07-24 17:30:09 +0900

    amplifyのユーザー作成と残りリセット回数機能追加

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -6,34 +6,24 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <ion-button href="/tabs/account-setting">アカウント設定</ion-button>
-      <ion-button @click="createFunction(message)"
-        >アカウント作成テスト</ion-button
+      <ion-button href="/tabs/account-setting"
+        >アカウント作成ページへ</ion-button
       >
-      <ion-button @click="updateFunction">アップデートテスト</ion-button>
-    </ion-content>
-    <ion-content>
-      <ion-item>
-        <ion-label position="floating">名前を入力してください</ion-label>
-        <ion-input v-model="message"></ion-input>
-      </ion-item>
-    </ion-content>
-    <ion-content>
-      <ion-button @click="getDatas">ローカル呼び出しテスト</ion-button>
-      {{ testlocal }}
-    </ion-content>
-    <ion-content>
-      <ul id="example-1" style="list-style: none;">
+      <br />
+      <ion-label>
+        ユーザー一覧
+      </ion-label>
+      <ul style="list-style: none;">
         <li v-for="item in test" :key="item.id">
           <ion-card>
             <ion-card-content>
-              id: {{ item.id }} <br />
-              名前: {{ item.name }} <br />
-              残り回数: {{ item.count }} <br />
-              <ion-button @click="setData(item.name, item.id, item.count - 1)"
-                >データ格納テスト</ion-button
+              {{ item.name + "さん" }} 残り回数{{ item.count + "回" }}
+              <ion-button @click="updateFunction(item.id)"
+                >残り回数リセット</ion-button
               >
-              <ion-button @click="getDatas">ローカル取得動作確認</ion-button>
+              <ion-button @click="setDatas(item.id, item.name, item.count)"
+                >買い物するユーザーを選択します</ion-button
+              >
             </ion-card-content>
           </ion-card>
         </li>
@@ -50,11 +40,9 @@
     IonTitle,
     IonContent,
     IonButton,
-    IonCardContent,
-    IonCard,
-    IonInput,
-    IonItem,
     IonLabel,
+    IonCard,
+    IonCardContent,
   } from "@ionic/vue";
   import { createTodo, updateTodo } from "@/graphql/mutations";
   import { API, graphqlOperation } from "aws-amplify";
@@ -62,7 +50,7 @@
   import { ListTodosQuery } from "@/API";
   import { Plugins } from "@capacitor/core";
   import { ref } from "vue";
-  import Test from "../../types/testInterface";
+
   const { Storage } = Plugins;
 
   export default {
@@ -74,57 +62,65 @@
       IonContent,
       IonPage,
       IonButton,
-      IonCardContent,
-      IonCard,
-      IonInput,
-      IonItem,
+
       IonLabel,
+      IonCard,
+      IonCardContent,
     },
     data: function() {
       return {
-        storageData: null,
+        storageData: "",
       };
     },
     setup() {
-      const test = ref("");
-      const message = ref("");
-      const id = ref(0);
+      const test = ref([]);
       const name = ref("");
 
       // eslint-disable-next-line vue/no-dupe-keys
-      return { test, message, id, name };
+      return { test, name };
     },
-    async created(this: { test: any }) {
-      const sample = await API.graphql(graphqlOperation(listTodos));
-      if ("data" in sample && sample.data) {
-        const post = sample.data as ListTodosQuery;
-        if (post !== undefined) {
-          this.test = post.listTodos?.items;
-        }
-      }
+    created() {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      this.getTodos();
     },
     methods: {
-      async createFunction(message: string) {
+      async createFunction(name: string) {
         await API.graphql(
           graphqlOperation(createTodo, {
             input: {
-              name: message,
+              name: name,
               count: 10,
             },
           })
         );
+        location.href = "/tabs/tab3";
       },
-      async updateFunction() {
+      async updateFunction(id: string) {
         await API.graphql(
           graphqlOperation(updateTodo, {
             input: {
-              id: "test",
-              name: "matsumoto kazumasa",
+              id: id,
+              count: 10,
             },
           })
         );
+        location.href = "/tabs/tab3";
+      },
+      async getTodos(this: { test: any }) {
+        console.log("test");
+        const sample = await API.graphql(graphqlOperation(listTodos));
+        if ("data" in sample && sample.data) {
+          const post = sample.data as ListTodosQuery;
+          if (post !== undefined) {
+            this.test = post.listTodos?.items;
+          }
+        }
       },
       async setDatas(user: string, id: string, count: number) {
+        console.log(id);
+        console.log(user);
+        console.log(count);
         await Storage.set({
           key: id,
           value: JSON.stringify({
@@ -132,26 +128,6 @@
             count: count,
           }),
         });
-      },
-      async getDatas() {
-        const ret = await Storage.get({ key: "test" });
-        const testlocal = JSON.parse(ret.value!) as Test;
-        console.log(testlocal);
-        return testlocal;
-      },
-      async setData() {
-        const date = new Date();
-        await Storage.set({
-          key: "user",
-          value: JSON.stringify({
-            id: 1,
-            time: date,
-          }),
-        });
-      },
-      async getData() {
-        const ret = await Storage.get({ key: "user" });
-        this.storageData = JSON.parse(ret.value!);
       },
     },
   };
--- src/views/Transaction/Transaction.vue
+++ src/views/Transaction/Transaction.vue
@@ -6,41 +6,60 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <ion-label>
-        トランザクションページ
-      </ion-label>
+      <ion-label> こんにちは{{ name }}さん </ion-label>
+      <br />
       <ion-button @click="sampleFunction">送信</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonLabel,
-  IonButton
-} from "@ionic/vue";
-import { multisigTransaction } from "../../util/symbol";
-
-export default {
-  name: "Tab1",
-  components: {
+  import {
+    IonPage,
     IonHeader,
     IonToolbar,
     IonTitle,
-    IonPage,
     IonContent,
     IonLabel,
-    IonButton
-  },
-  methods: {
-    sampleFunction: function() {
-      multisigTransaction().then();
-    }
-  }
-};
+    IonButton,
+  } from "@ionic/vue";
+  import { multisigTransaction } from "../../util/symbol";
+  import { ref } from "@vue/reactivity";
+
+  import { Plugins } from "@capacitor/core";
+  const { Storage } = Plugins;
+
+  export default {
+    name: "Tab1",
+    components: {
+      IonHeader,
+      IonToolbar,
+      IonTitle,
+      IonPage,
+      IonContent,
+      IonLabel,
+      IonButton,
+    },
+    setup() {
+      const name = ref("");
+
+      return { name };
+    },
+    create(this: { name: any }) {
+      console.log(this.name);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      this.getUserId();
+    },
+    methods: {
+      sampleFunction: function() {
+        multisigTransaction().then();
+      },
+      async getUserId() {
+        console.log("getUserId");
+        const ret = await Storage.get({ key: "user" });
+        console.log(ret);
+      },
+    },
+  };
 </script>
